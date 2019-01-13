71eb57843eade14f827c5c9382b3feb7ebfbe926
tmsss <dissid3nt.mail@gmail.com>
2019-01-13 16:51:52 +0000

    clean tweets function added to regex utils

--- regex_utils.py
+++ regex_utils.py
@@ -1,5 +1,12 @@
 import re
 
+def clean_tweet(tweet):
+    '''
+    Utility function to clean tweet text by removing links, special characters
+    using simple regex statements.
+    '''
+    return ' '.join(re.sub("((RT)|(rt)|@[A-Za-z0-9]+)|([^0-9A-Za-z \t])|(\w+:\/\/\S+)", " ", tweet).split())
+    
 
 def get_url(url, **kwargs):
     # sep = '\\'  # remove everything after separator
